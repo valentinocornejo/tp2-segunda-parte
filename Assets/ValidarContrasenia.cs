c3f91aa768ebb2e2efa6f43173c16829b0084622
valentinocornejo <101400293+valentinocornejo@users.noreply.github.com>
2022-04-24 19:36:43 -0300

    Initial commit

---
+++ Assets/ValidarContrasenia.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+
+    // Start is called before the first frame update
+    void Start()
+
+
+    {
+        contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+}
+
